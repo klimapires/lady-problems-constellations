c32d16c63d7b8893d8d5a383c4deb8ba9e7f98ab
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2016-11-05 21:30:49 -0200

    páginas com extends

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url, include
 from website import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^quero-trabalhar/$', views.quero_trabalhar, name='quero-trabalhar'),
+    url(r'^quero-contratar/$', views.quero_contratar, name='quero-contratar'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/index.html', {})
+
+def quero_trabalhar(request):
+    return render(request, 'website/quero_trabalhar.html', {})
+
+def quero_contratar(request):
+    return render(request, 'website/quero_contratar.html', {})
